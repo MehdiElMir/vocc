c7663068c6d3eba9d34dd98a372cea326fbfdd4b
midrissi01 <m.oulhaj_idrissi@mundiapolis.ma>
2023-06-20 14:40:44 +0100

    apropos

---
+++ apropos.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="lib/bootstrap5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>index</title>
+</head>
+<body>
+    <?php include('_header.php'); ?>    
+    <div class= "container">
+        <div class= "row align-items-center justify-content-start">
+            <div class= " namezone">
+                <p> A PROPOS</p>
+            </div>
+        </div>
+    </div>
+   
+
+    <footer>
+        <?php include('_footer.php');?>
+    </footer>
+
+    <script src="lib/jquery.min.js"></script>
+    <script src="lib/bootstrap5/js/bootstrap.bundle.js"></script>
+    <script src="js/index.js"></script>
+</body>
+
+</html>
