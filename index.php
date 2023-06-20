347e46cc07b190bbe0a43abab52b66535e7db05e
MehdiElMir <m.elmir.dev@gmail.com>
2023-06-20 09:58:23 +0100

    organize docs

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="lib/bootstrap5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>index</title>
+</head>
+<body>
+    
+    <h1>hello</h1>
+
+    <script src="lib/jquery.min.js"></script>
+    <script src="lib/js/bootstrap.js"></script>
+    <script src="js/index.js"></script>
+</body>
+
+</html>
--- js/index.js
+++ js/index.js
@@ -1,1 +1,2 @@
 alert('teeest');
+$("body").html("<h1>test</h1>");
